0f684a8c1fc12492851f0f7f77fc0e2ee816edfd
Rene van Spronsen <r.v.spronsen@dji.minjus.nl>
2020-12-30 19:01:16 +0100

    Add ffmpeg conversions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,12 +6,14 @@ import {
   UseInterceptors,
   UploadedFile,
   Query,
+  Body,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as path from 'path';
 import * as cmd from 'node-command-line';
 import * as fs from 'fs';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApngUploadDto } from './Dto/ApngUploadDto';
 
 @Controller()
 export class AppController {
@@ -22,51 +24,132 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('download')
-  async downloadFile(
-    @Query('name') name,
-    @Query('framerate') framerate: number,
-    @Query('loop') loop: boolean,
-    @Res() res,
-  ): Promise<void> {
-    // unpack zip
-    const unpackRes = await cmd.run(
-      `unzip -q /app/files/${name} -d /app/files/${name}-files`,
-    );
-
-    if (unpackRes.success) {
-      await fs.readdir(`/app/files/${name}-files`, async (err, items) => {
-        // set fileName and loop variable for images
-        const fileNumbers = items[items.length - 1].match(/(\d+)/).length;
-        const fileName = items[items.length - 1].split(/(\d)/)[0];
-
-        // convert PNG's to APNG
-        await cmd.run(
-          `ffmpeg -r ${framerate} -i /app/files/${name}-files/${fileName}%0${fileNumbers}d.png -plays ${
-            loop ? 0 : 1
-          } /app/downloads/${name}.apng`,
-        );
-
-        // check if APNG exists & return fileStream of APNG
-        const file = await path.basename(`/app/downloads/${name}.apng`);
-        if (file) {
-          const reader = await fs.createReadStream(
-            `/app/downloads/${name}.apng`,
-          );
-          reader.pipe(res);
-        }
+  @Get('gif/download')
+  async downloadFileGif(@Query('name') name, @Res() res): Promise<void> {
+    const file = await path.basename(`/app/downloads/${name}.gif`);
+    if (file) {
+      res.set({
+        'Content-Type': 'image/gif',
       });
+      await res.sendFile(`downloads/${name}.gif`, { root: './' });
     }
   }
 
-  @Post('upload')
+  @Post('gif/upload')
   @UseInterceptors(FileInterceptor('image'))
-  async uploadedFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadedFileGif(
+    @UploadedFile() file,
+    @Body() apngUploadDto: ApngUploadDto,
+  ) {
     const response = {
       originalname: file.originalname,
       filename: file.filename,
     };
+
+    const unpackRes = await cmd.run(
+      `unzip -q /app/files/${file.filename} -d /app/files/${file.filename}-files`,
+    );
+
+    if (unpackRes.success) {
+      const converted = await this.appService.convertSequence(
+        apngUploadDto,
+        file,
+        'gif',
+      );
+      if (!converted) {
+        response['error'] = 'Files failed converting';
+      }
+    } else {
+      fs.unlink(`/app/files/${file.filename}`, (err) => console.log(err));
+      response['error'] = 'File is not a ZIP!';
+    }
+    return response;
+  }
+
+  @Get('apng/download')
+  async downloadFileApng(@Query('name') name, @Res() res): Promise<void> {
+    const file = await path.basename(`/app/downloads/${name}.apng`);
+    if (file) {
+      res.set({
+        'Content-Type': 'image/apng',
+      });
+
+      await res.sendFile(`downloads/${name}.apng`, { root: './' });
+    }
+  }
+
+  @Post('apng/upload')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadedFileApng(
+    @UploadedFile() file,
+    @Body() apngUploadDto: ApngUploadDto,
+  ) {
+    const response = {
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+
+    const unpackRes = await cmd.run(
+      `unzip -q /app/files/${file.filename} -d /app/files/${file.filename}-files`,
+    );
+
+    if (unpackRes.success) {
+      const converted = await this.appService.convertSequence(
+        apngUploadDto,
+        file,
+        'apng',
+      );
+      if (!converted) {
+        response['error'] = 'Files failed converting';
+      }
+    } else {
+      fs.unlink(`/app/files/${file.filename}`, (err) => console.log(err));
+      response['error'] = 'File is not a ZIP!';
+    }
+    return response;
+  }
+
+  @Get('webp/download')
+  async downloadFileWebp(@Query('name') name, @Res() res): Promise<void> {
+    const file = await path.basename(`/app/downloads/${name}.webp`);
+    if (file) {
+      res.set({
+        'Content-Type': 'image/webp',
+      });
+
+      await res.sendFile(`downloads/${name}.webp`, { root: './' });
+    }
+  }
+
+  @Post('webp/upload')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadedFileWebp(
+    @UploadedFile() file,
+    @Body() apngUploadDto: ApngUploadDto,
+  ) {
+    const response = {
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+
+    const unpackRes = await cmd.run(
+      `unzip -q /app/files/${file.filename} -d /app/files/${file.filename}-files`,
+    );
+
+    if (unpackRes.success) {
+      const converted = await this.appService.convertSequence(
+        apngUploadDto,
+        file,
+        'webp',
+        '-vcodec libwebp -lossless 1 -qscale 100 -preset default -an -vsync 0',
+      );
+      if (!converted) {
+        response['error'] = 'Files failed converting';
+      }
+    } else {
+      fs.unlink(`/app/files/${file.filename}`, (err) => console.log(err));
+      response['error'] = 'File is not a ZIP!';
+    }
     return response;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import * as cmd from 'node-command-line';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async convertSequence(
+    apngUploadDto,
+    file,
+    ext: string,
+    options: string = '',
+  ): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      await fs.readdir(
+        `/app/files/${file.filename}-files`,
+        async (err, items: string[]) => {
+          const fileNumbers = items[items.length - 1].match(/(\d+)/).length;
+          const fileName = items[items.length - 1].split(/(\d)/)[0];
+
+          const convert = await cmd.run(
+            `ffmpeg -r ${apngUploadDto.framerate} -i /app/files/${
+              file.filename
+            }-files/${fileName}%0${fileNumbers}d.png ${options} -plays ${
+              apngUploadDto.loop ? 0 : 1
+            } /app/downloads/${file.filename}.${ext}`,
+          );
+
+          if (!convert.success) {
+            reject(
+              'Make sure ZIP root files are name numbered PNGs. someImage0001.png',
+            );
+          }
+
+          fs.unlink(`/app/files/${file.filename}`, err => console.log(err));
+          fs.rmdir(
+            `/app/files/${file.filename}-files`,
+            { recursive: true },
+            err => reject(err),
+          );
+
+          resolve(convert.success);
+        },
+      );
+    });
   }
 }
